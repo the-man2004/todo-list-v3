cbfb6b41a1c570a69dfea6eabccbd4dca5e76d5a
the-man2004 <heinrichhb111@gmail.com>
2023-06-09 12:52:49 -0700

    Added form submit functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import React, { useState } from "react";
 import "./App.css";
+import Form from "./components/form/Form";
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      todo: "Hire me!",
+      id: 0.1134366111343661,
+    },
+    {
+      todo: "Eat a Donut",
+      id: 0.0027448200274482,
+    },
+  ]);
+
+  console.log(todos);
+
+  const addTodoHandler = (newTodo) => {
+    setTodos((prevTodos) => [...prevTodos, newTodo]);
+  };
+
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <React.Fragment>
+      <Form onSubmit={addTodoHandler} />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/form/Form.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+import classes from "./Form.module.css";
+
+const Form = (props) => {
+  // State for remembering the input value
+  const [todo, setTodo] = useState("");
+
+  // input change handler function
+  const changeHandler = (event) => {
+    setTodo(event.target.value);
+  };
+
+  // form submit handler function
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+
+    if (todo !== "") {
+      props.onSubmit({
+        todo: todo,
+        id: Math.random(),
+      });
+
+      setTodo("");
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className={classes["form-wrapper"]}>
+        <h1>Hello from form!</h1>
+        <form onSubmit={formSubmitHandler}>
+          <input
+            onChange={changeHandler}
+            value={todo}
+            type="text"
+            placeholder="Add Todo"
+          />
+          <button className={classes.btn} type="submit">
+            +
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Form;
